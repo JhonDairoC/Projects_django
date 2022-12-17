6cc7f1df49cf15c83f982b12a4b4b81e70c22f72
JC <101678630+JhonDairoC@users.noreply.github.com>
2022-12-17 10:42:21 -0500

    Delete newspaper directory
    Se eliminan archivos de la rama main

--- newspaper/accounts/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from accounts import views
-from .views import *
-
-urlpatterns = [
-    path('signup/', SingUpView.as_view(), name='signup'),
-]
--- newspaper/pages/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView
-# Create your views here.
-
-class HomePageView(TemplateView):
-    template_name= 'home.html'
-
-class ErrorView(TemplateView):
-    template_name= 'error.html'
